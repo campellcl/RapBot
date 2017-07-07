e04432393baa736b95598fed04896dce8f81e4ac
Chris Campell <ccampell.asu@gmail.com>
2017-07-07 16:45:35 -0400

    Partial commit of unfinished PhoneticTranscriber. Added important TODO's. Added a new method which detects the chorus of a song based on the placement of a [chorus] tag and the presence of an escape character or empty line.
    Signed-off-by: Chris Campell <ccampell.asu@gmail.com>

--- Program/PhoneticTranscriber/PhoneticTranscriber.py
+++ Program/PhoneticTranscriber/PhoneticTranscriber.py
@@ -6,10 +6,91 @@ import sys
 import os.path
 import re
 import nltk
+import json
 from nltk.corpus import cmudict
+from enum import Enum
+from collections import OrderedDict
 
 __author__ = "Chris Campell"
-__version__ = "4/21/2017"
+__created__ = "4/21/2017"
+__version__ = "7/06/2017"
+
+
+class ScraperStatus(Enum):
+    """
+    ScraperStatus: An Enumerated type representing the status of the this artist in the information retrieval pipeline.
+    Status assignments proceed as follows:
+     stage_zero: Artist metadata has been retrieved via target_artists.json or just fetched via web-scraper. A new
+        directory for the artist has not been created yet and the information persists only in memory.
+     stage_one: Artist metadata has been retrieved via target_artists.json. A new directory for information retrieved
+        about the artist has been created under Data\OHLA\Artists.
+     stage_two: Album metadata (excluding songs) has been retrieved via web-scraper. A new directory for information
+        retrieved about the album's songs has been created as Data\OHLA\Artists\<ARTIST_NAME>\<ALBUM_NAME>.
+     stage_three: Song HTML information has been retrieved but not yet parsed. HTML data has been stored at the
+        directory Data\OHLA\Artists\<ARTIST_NAME>\<ALBUM_NAME>\<SONG_NAME>.
+     stage_four: Song lyrics have been parsed into plain-text. Automatically generated transcriptions have not yet
+        been performed.
+    """
+    stage_zero = 0
+    stage_one = 1
+    stage_two = 2
+    stage_three = 3
+    stage_four = 4
+
+    def __eq__(self, obj):
+        # Attempt to type cast the comparison object to this class:
+        try:
+            scraper_status_obj = ScraperStatus(obj)
+        except Exception as exc:
+            print("ScraperStatus: Failure to typecast foreign object during comparison. Exception states: %s"
+                  % exc.__cause__)
+        if type(obj) is type(self):
+            return self.value == scraper_status_obj.value
+
+
+class EnumEncoder(json.JSONEncoder):
+    """
+    EnumEncoder: This class enables the ScraperStatus Enum to be JSON encodable. This method is only called as a hook
+     via json.dump().
+    :source: https://stackoverflow.com/questions/24481852/serialising-an-enum-member-to-json
+    """
+    def default(self, obj):
+        if type(obj) is ScraperStatus:
+            # str(obj) is of type: 'ScraperStatus.stage_zero'
+           return {"__enum__": str(obj)}
+        return json.JSONEncoder.default(self, obj)
+
+
+def enum_decoder(dict):
+    """
+    enum_decoder: This method enables the ScraperStatus Enum to be JSON decodable (read with json.load()).
+    :param dict: The dictionary returned by json.load, this method is called only as a hook.
+    :return attribute: The ScraperStatus.member that the provided string actually belongs to.
+    :source: https://stackoverflow.com/questions/24481852/serialising-an-enum-member-to-json
+    """
+    if "__enum__" in dict:
+        name, member = dict["__enum__"].split(".")
+        return getattr(ScraperStatus, member)
+    else:
+        return OrderedDict(dict)
+
+
+def load_web_scraper_target_urls(target_artists_loc):
+    """
+    load_web_scraper_target_urls: Helper method, loads the list of artists, albums, and their associated URLs
+        into memory. The web-scraper will use this target list of URLs to scrape the song information for each album.
+    :param target_artists_loc: The pre-validated (file_exists(fpath)) storage directory of the json file containing
+     the list of artists, unique identifiers, and target urls for scraping.
+    :return target_artists: A dictionary of artists sorted by unique identifier AID (assigned by order of encounter),
+     and their associated URL's for the web-scraper to target.
+    """
+    with open(target_artists_loc, 'r') as fp:
+        # Load the target_artists.json file as an OrderedDictionary, hook into custom enum decoder for ScraperStatus:
+        target_artists_string_dict = json.load(fp=fp, object_hook=enum_decoder)
+        # print("Init: Success! Target URL's loaded into memory. Converting back to integer representation.")
+        target_artists = {int(k): v for k, v in target_artists_string_dict.items()}
+    return target_artists
+
 
 def main(download_new_corpus, storage_dir):
     """
@@ -18,34 +99,38 @@ def main(download_new_corpus, storage_dir):
      2. Downloading new corpora for use by NLTK (if neccessary)
      3. Encoding metadata such as (song name, artist name, album, g2p accuracy, etc...) in JSON form
      4. Writing G2P transcriptions and metadata to output directory in JSON form
-    :param DownloadNewCorpus:
-    :return:
+    :param download_new_corpus: A boolean flag indicating if a new CMUDict corpus should be fetched from the external
+        server.
+    :return None: Upon completion, TODO: method header.
     """
     if download_new_corpus:
         nltk.download_gui()
         # Print the paths that NLTK uses for Corpora: print(nltk.data.path)
         sys.exit(0)
-    # Read Plaintext from file:
-    # Online HipHop Lyrics Archive PlainText Storage Location:
-    plain_text_file = storage_dir + '\\OHLA\\PlainText\\Eric B and Rakim\\Paid in Full.txt'
-    with open(plain_text_file, 'r') as fp:
-        plain_text = fp.read()
-    ''' Tokenize Plaintext '''
-    # Tokenize Lines:
-    tokenized_lines = tokenize_lines(plain_text)
-    # Tokenize Words:
-    tokenized_words = tokenize_words(plain_text)
-    # Normalize tokens:
-    tokenized_words = [word.lower() for word in tokenized_words]
-    # Convert tokenized text to NLTK Text object:
-    # tokenized_words = nltk.Text(tokens=tokenized_words)
-    ''' Perform Grapheme to Phoneme (G2P) transcription in ARPABET'''
-    arpabet_cmu_graphones, failed_transcriptions = transcribe_arpabet_via_cmu(tokenized_words)
-    ''' Build G2P Transcription Statistics and Metadata'''
-    g2p_json_encoding = {}
-    # TODO: Obtain song metadata. Obtain g2p transcription statistics.
-    ''' Write G2P transcription to storage directory '''
-    # TODO: Write converted words to storage directory in json format. Attach transcription statistics.
+    ''' Read Plaintext from Files '''
+    for aid, artist_info in target_artists.items():
+        for alid, album_info in artist_info['albums'].items():
+            for sid, song_info in album_info['songs'].items():
+                song_ascii = song_info['ascii']
+                ''' Tokenize Plaintext '''
+                # Tokenize Lines:
+                tokenized_lines = tokenize_lines(song_ascii)
+                # The first four lines of the song are always the header, so exclude the meta-information:
+                tokenized_lines = tokenized_lines[4:]
+                # Tokenize Words:
+                tokenized_words = tokenize_words(tokenized_lines)
+                # Normalize tokens:
+                tokenized_words = [word.lower() for word in tokenized_words]
+                # Convert tokenized text to NLTK Text object:
+                # tokenized_words = nltk.Text(tokens=tokenized_words)
+                ''' Perform Grapheme to Phoneme (G2P) transcription in ARPABET'''
+                arpabet_cmu_graphones, failed_transcriptions = transcribe_arpabet_via_cmu(tokenized_words)
+                ''' Build G2P Transcription Statistics and Metadata'''
+                g2p_json_encoding = {}
+                # TODO: Obtain song metadata. Obtain g2p transcription statistics.
+                ''' Write G2P transcription to storage directory '''
+                # TODO: Write converted words to storage directory in json format. Attach transcription statistics.
+
 
 def transcribe_arpabet_via_cmu(tokenized_words):
     """
@@ -73,6 +158,44 @@ def transcribe_arpabet_via_cmu(tokenized_words):
             failed_transcriptions.append(token)
     return arpabet_graphones, failed_transcriptions
 
+
+def identify_chorus_lines(tokenized_lines_with_spaces):
+    """
+    identify_chorus_lines: Given a list of plaintext lines in a song (with deliminating '' still present), this method
+    will locate the section of the song that constitutes the chorus and return it.
+    :param tokenized_lines_with_spaces: A list of plaintext lyrics deliminated by \n character with "" still present as
+        sometimes the only way to identify the end of the chorus is via blank spaces "".
+    :return chorus: An OrderedDictionary that represents the lines in the song (and their associated line numbers in
+        the provided 'tokenized_lines_with_spaces'). Takes the form:
+         :key line_num: An index corresponding to the line in the provided list.
+         :value line: The plaintext/lyrics in the list corresponding to the line_number.
+    """
+    chorus = OrderedDict()
+    # As far as I can tell songs will mark the end of the chorus based on '' or [Verse #]:
+    chorus_start_line_index = None
+    chorus_end_line_index = None
+    for line_num, line in enumerate(tokenized_lines_with_spaces):
+        normalized_line = str.lower(line)
+        # Identify the line number that indicates the start of the chorus:
+        if 'chorus' in normalized_line:
+            chorus_start_line_index = line_num + 1
+            break
+    # Identify the line number that indicates the end of the chorus:
+    if chorus_start_line_index is not None:
+        for line_num, line in enumerate(tokenized_lines_with_spaces[chorus_start_line_index:]):
+            normalized_line = str.lower(line)
+            # The next '' or [Verse #] following the chorus marks the end of the chorus:
+            if '[verse' in normalized_line or normalized_line == '':
+                chorus_end_line_index = line_num + chorus_start_line_index
+                break
+    # Capture the text that constitutes the chorus:
+    line_num = chorus_start_line_index
+    for chorus_line in tokenized_lines_with_spaces[chorus_start_line_index:chorus_end_line_index]:
+        chorus[line_num] = chorus_line
+        line_num += 1
+    return chorus
+
+
 def tokenize_lines(plain_text):
     """
     tokenize_lines -Accepts the lyrics of a song as a string and partitions the string into a list of lines.
@@ -81,14 +204,23 @@ def tokenize_lines(plain_text):
     """
     # Utilize str.split() to partition on '\n' character:
     tokenized_lines = str.split(plain_text, sep='\n')
+    ''' Identify the lines in the song that constitute the Chorus '''
+    chorus = identify_chorus_lines(tokenized_lines_with_spaces=tokenized_lines)
+    # TODO: The line numbers recorded in chorus are from the tokenized lines with spaces. The numbers will all shift
+    # once the spaces are removed. How can I efficently update the line indices?
+    tokenized_lines = [line for line in tokenized_lines if line is not ""]
+    # TODO: How to handle noise like [Applause] and 'lyrics 4x'
+    # Mark the line number associated with a [Chorus] tag and then
     return tokenized_lines
 
-def tokenize_words(plain_text):
+
+def tokenize_words(line_deliminated_lyrics):
     """
-    tokenize_words -Accepts the lyrics of a song as a string and partitions the string into a list of words.
-    :param plain_text: The string containing song lyrics read from plaintext or HTML.
-    :return tokenized_words: A list of words composed using the provided string.
+    tokenize_words -Accepts the lyrics of a song as a list of lines and seperates them into a list of words.
+    :param tokenized_lyrics: The list of song lyrics read from plaintext.
+    :return tokenized_words: A list of words composed using the provided list of lyrics.
     """
+    tokenized_lyrics = OrderedDict()
     # Utilize Regular expression to partition on ' ' and '\n':
     # tokenized_words = re.split(' |\n', plain_text)
     '''
@@ -96,18 +228,27 @@ def tokenize_words(plain_text):
     partition on spaces OR new-line characters and ignore apostrophes
     For more information: http://stackoverflow.com/questions/2596893/regex-to-match-words-and-those-with-an-apostrophe
     '''
-    #tokenized_words = re.split(pattern=r'\s|\n|,|(?=.*\w)^(\w|\')+$',string=plain_text)
-    tokenized_words = re.split(pattern=r'\s|\n|,',string=plain_text)
-    # Remove 'None' tokens:
-    tokenized_words = [token for token in tokenized_words if token is not None]
-    # Remove '' tokens:
-    tokenized_words = [token for token in tokenized_words if token is not '''''']
-    # tokenized_words = [token if token is not None else token for token in tokenized_words]
-    return tokenized_words
+    for line_num, line in enumerate(line_deliminated_lyrics):
+        #tokenized_words = re.split(pattern=r'\s|\n|,|(?=.*\w)^(\w|\')+$',string=plain_text)
+        tokenized_words = re.split(pattern=r'\s|\n|,', string=line)
+        # Remove 'None' tokens:
+        tokenized_words = [token for token in tokenized_words if token is not None]
+        # Remove '' tokens:
+        tokenized_words = [token for token in tokenized_words if token is not '''''']
+        # tokenized_words = [token if token is not None else token for token in tokenized_words]
+        tokenized_lyrics[line_num] = tokenized_words
+    return tokenized_lyrics
+
 
 if __name__ == '__main__':
-    storage_dir = os.path.abspath(os.path.join(
+    # Initialize storage paths:
+    global_storage_dir = os.path.abspath(os.path.join(
         os.path.dirname(__file__), '../..', 'Data/'
     ))
-    # If the DownloadNewCorpus flag is set; script will execute nltk download manager then terminate.
-    main(download_new_corpus=False, storage_dir=storage_dir)
+    target_artists_loc = global_storage_dir + "\\OHLA\\WebScraper\\MetaData\\target_artists_stage_three.json"
+    # Load required target_artists.json into memory:
+    print("PT[Init]: Loading artist file into memory (approx. 154 MB) please be patient...")
+    target_artists = load_web_scraper_target_urls(target_artists_loc=target_artists_loc)
+    print("PT[Init]: Metadata loaded into memory. Proceeding to text pre-processing via tokenization.")
+    # Note: If the download_new_corpus flag is set; script will execute nltk download manager then terminate.
+    main(download_new_corpus=False, storage_dir=global_storage_dir)
